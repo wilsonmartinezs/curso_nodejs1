d1c65486e7cef4e227fc0481efe595159ae73699
Wilson <wicho80@gmail.com>
2020-05-03 08:42:22 -0500

    Se crea el ejercicio process con parametros

---
+++ process.js
@@ -0,0 +1,13 @@
+console.log(process.argv);
+function leerOpcion(opcion){
+var index=process.argv.indexOf(option);
+if(ind===-1){
+return false;
+}else{
+   return process.argv[ind+1];
+}
+var  nombre=leerOpcion("-nombre");
+
+console.log("El nombre es : "+nombre);
+
+}
