65b2e9ad8acb457d48cb306fe19e4b82366031af
Wilson <wicho80@gmail.com>
2020-05-03 08:21:05 -0500

    Se crea el ejercicio del comando consoleº:

---
+++ console.js
@@ -0,0 +1,8 @@
+var nombre="Ricardo Bernal";
+var numero=10;
+var json={Nombre:"Wilson MArtinez Saldarriaga"}
+console.log('%j',json);
+console.log(`Eln nombre es: ${nombre} y el numero es ${numero}`);
+
+console.log(__filename);
+//consloe.log(__dirname);
